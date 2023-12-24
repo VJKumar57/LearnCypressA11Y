f5a9c13d00121ac1b1bcea14e313b3da50781b70
VJKumar57 <154102218+VJKumar57@users.noreply.github.com>
2023-12-23 21:36:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,31 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // ... (existing code) ...
 });
+
+function submitQuiz() {
+    // Retrieve selected answers
+    const answer1 = document.querySelector('input[name="q1"]:checked');
+    const answer2 = document.querySelector('input[name="q2"]:checked');
+
+    // Check if all questions are answered
+    if (!answer1 || !answer2) {
+        alert("Please answer all questions.");
+        return;
+    }
+
+    // Check answers and display result
+    const resultContainer = document.getElementById("result");
+    let correctCount = 0;
+
+    if (answer1.value === "Paris") {
+        correctCount++;
+    }
+
+    if (answer2.value === "Jupiter") {
+        correctCount++;
+    }
+
+    resultContainer.textContent = `You got ${correctCount} out of 2 questions correct.`;
+}
+
+document.getElementById("submit-btn").addEventListener("click", submitQuiz);
