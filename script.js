fa49dc2daabcecd5b5ac029437eae9b3784dffde
VJKumar57 <154102218+VJKumar57@users.noreply.github.com>
2023-12-23 21:54:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // ... (existing code) ...
-
-    // Add smooth scrolling for navigation links
-    const navLinks = document.querySelectorAll('nav a');
-
-    navLinks.forEach(link => {
-        link.addEventListener('click', function(e) {
-            e.preventDefault();
-
-            const targetId = this.getAttribute('href').substring(1);
-            const targetElement = document.getElementById(targetId);
-
-            window.scrollTo({
-                top: targetElement.offsetTop - 50, // Adjust for fixed header
-                behavior: 'smooth'
-            });
-        });
-    });
-
-    // ... (existing code) ...
-});
-
 function submitQuiz() {
     // Retrieve selected answers
     const answer1 = document.querySelector('input[name="q1"]:checked');
@@ -46,10 +23,5 @@ function submitQuiz() {
 
     resultContainer.textContent = `You got ${correctCount} out of 2 questions correct.`;
 }
-
-document.getElementById("quiz-btn").addEventListener("click", function() {
-    // Navigate to the quiz page
-    window.location.href = "quiz.html";
-});
 
 document.getElementById("submit-btn").addEventListener("click", submitQuiz);
