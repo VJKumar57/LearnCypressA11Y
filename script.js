4479b822e349c704dd951f88afe09522f3b07b42
VJKumar57 <154102218+VJKumar57@users.noreply.github.com>
2023-12-25 00:55:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -101,8 +101,8 @@ function retakeQuiz() {
 }
 
 function submitQuiz() {
-    displayResult();
     alert("Quiz submitted Successfully.  Please find your score below!");
+    displayResult();
     // Additional logic for submitting the quiz to a server or storing results.
     document.getElementById("retake-btn").style.display = "block"; // Display the "Retake Quiz" button
 }
