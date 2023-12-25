a59af6e9f3986eda485845bd5564af2bb7751962
VJKumar57 <154102218+VJKumar57@users.noreply.github.com>
2023-12-25 00:06:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,95 @@
-function submitQuiz() {
-    // Retrieve selected answers
-    const answer1 = document.querySelector('input[name="q1"]:checked');
-    const answer2 = document.querySelector('input[name="q2"]:checked');
-    const answer3 = document.querySelector('input[name="q3"]:checked');
+// Sample quiz questions and answers
+const quizData = [
+    {
+        question: "What is the capital of France?",
+        options: ["Paris", "Berlin", "London", "Madrid"],
+        correctAnswer: "Paris"
+    },
+    // Add more questions here...
+];
 
-    // Check if all questions are answered
-    if (!answer1 || !answer2 || !answer3) {
-        alert("Please answer all questions.");
-        return;
-    }
+let currentQuestion = 0;
+let userAnswers = [];
+let attemptCount = 0;
+const maxAttempts = 3;
 
-    // Check answers and display result
-    const resultContainer = document.getElementById("result");
-    let correctCount = 0;
+function displayQuestion() {
+    const questionElement = document.getElementById("question");
+    const optionsElement = document.getElementById("options");
 
-    if (answer1.value === "Paris") {
-        correctCount++;
-    }
+    questionElement.textContent = `Question ${currentQuestion + 1}: ${quizData[currentQuestion].question}`;
+    optionsElement.innerHTML = "";
 
-    if (answer2.value === "Jupiter") {
-        correctCount++;
-    }
-
-    if (answer3.value === "Venus") {
-        correctCount++;
-    }
-
-    resultContainer.textContent = `You got ${correctCount} out of 3 questions correct.`;
+    quizData[currentQuestion].options.forEach((option, index) => {
+        const li = document.createElement("li");
+        li.innerHTML = `<input type="radio" name="answer" value="${option}"> ${option}`;
+        optionsElement.appendChild(li);
+    });
 }
 
-document.getElementById("submit-btn").addEventListener("click", submitQuiz);
+function nextQuestion() {
+    const selectedOption = document.querySelector('input[name="answer"]:checked');
 
-function resetForm() {
-    // Get the form element
-    var form = document.getElementById("myForm");
+    if (selectedOption) {
+        userAnswers.push(selectedOption.value);
+        currentQuestion++;
 
-    // Reset the form
-    form.reset();
+        if (currentQuestion < quizData.length) {
+            displayQuestion();
+        } else {
+            displayResult();
+        }
+    } else {
+        alert("Please select an option before moving to the next question.");
+    }
+}
+
+function displayResult() {
+    const resultContainer = document.getElementById("result-container");
+    const resultMessage = document.getElementById("result-message");
+
+    const score = calculateScore();
+
+    resultMessage.textContent = `You scored ${score} out of ${quizData.length}.`;
+
+    if (score >= quizData.length / 2) {
+        resultContainer.style.color = "green";
+        resultMessage.textContent += " Well done!";
+    } else {
+        resultContainer.style.color = "red";
+        resultMessage.textContent += " Keep practicing.";
+    }
+
+    document.getElementById("button-container").style.display = "none";
+    resultContainer.style.display = "block";
+}
+
+function calculateScore() {
+    let score = 0;
+
+    for (let i = 0; i < quizData.length; i++) {
+        if (userAnswers[i] === quizData[i].correctAnswer) {
+            score++;
+        }
+    }
+
+    return score;
+}
+
+function retakeQuiz() {
+    if (attemptCount < maxAttempts - 1) {
+        attemptCount++;
+        currentQuestion = 0;
+        userAnswers = [];
+        displayQuestion();
+        document.getElementById("button-container").style.display = "block";
+        document.getElementById("result-container").style.display = "none";
+    } else {
+        alert("You have reached the maximum number of attempts.");
+    }
+}
+
+function submitQuiz() {
+    alert("Quiz submitted!");
+    // Additional logic for submitting the quiz to a server or storing results.
 }
